b323db054f1833966d1711a01c991677d60983b6
Andreas Børsheim <andreas.borsheim@nb.no>
2023-10-10 14:39:37 +0200

    wip: mvp reverse proxy and example upstream server

--- main.go
+++ main.go
@@ -1,31 +1,88 @@
 package main
 
 import (
-	"net/http"
-	"os"
-
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/nlnwa/heimdall/pdp"
+	"net/http"
+	"net/url"
 )
 
 func main() {
+
 	e := echo.New()
-
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
 
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello from server")
-	})
+	playbackIndexUrl, err := url.Parse("http://localhost:8085/")
 
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("PORT")
-	if httpPort == "" {
-		httpPort = "8080"
+	if err != nil {
+		e.Logger.Fatal(err)
 	}
 
-	e.Logger.Fatal(e.Start(":" + httpPort))
+	targets := []*middleware.ProxyTarget{
+		{
+			URL: playbackIndexUrl,
+		},
+	}
+
+	// g := e.Group("/web")
+	e.Use(middlewarePdp)
+	e.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func middlewarePdp(next echo.HandlerFunc) echo.HandlerFunc {
+	pdpUrl := "http://localhost:8087/auth"
+	accRes := pdp.AccessResponse{}
+
+	return func(c echo.Context) error {
+		fmt.Println("from middlewarePdp")
+
+		accRec := pdp.AccessRequest{
+			Url:   c.QueryParam("url"),
+			Token: c.QueryParam("token"),
+		}
+
+		fmt.Printf("Generating AccessRequest %#+v", accRec)
+
+		b, err := json.Marshal(accRec)
+		if err != nil {
+			return fmt.Errorf("failed reading the request body %s", err)
+		}
+		body := bytes.NewReader(b)
+
+		req, err := http.NewRequest("POST", pdpUrl, body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+		if err != nil {
+			return fmt.Errorf("failed creating request %s", err)
+		}
+
+		client := &http.Client{}
+
+		response, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("failed sending request %s", err)
+		}
+
+		defer response.Body.Close()
+
+		err = json.NewDecoder(response.Body).Decode(&accRes)
+		if err != nil {
+			return fmt.Errorf("failed decoding response %s", err)
+		}
+
+		if accRes.Permission == pdp.Deny {
+			return echo.NewHTTPError(http.StatusForbidden, "Access denied")
+		} else if accRes.Permission == pdp.Allow {
+			fmt.Println("...Access granted")
+			return next(c)
+		}
+		return next(c)
+	}
 }
